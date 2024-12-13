20afc088ab875d9cf79c12d162dd0a9107ea4ab0
hyeyeoung <rlagpdud0524@gmail.com>
2024-12-13 12:33:30 +0900

    Add project files.

---
+++ SF_1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SF_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent(); // -> Form1.designe.cs로 이동
+
+            //  MessageBox.Show("Hello");
+            /*
+             * 
+             */
+            textBox_print.Text = "이것은 \r\n멀티라인\r\n텍스트박스\r\n인데요."; // 줄 바꾸고 싶으면 \r\n사용 필수
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
